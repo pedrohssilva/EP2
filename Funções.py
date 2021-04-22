939daf7079e06a2fec9952776f5fa4938b05a209
pedrohssilva <81188551+pedrohssilva@users.noreply.github.com>
2021-04-22 17:51:01 -0300

    Update and rename README.py to Funções.py

--- Funções.py
+++ Funções.py
@@ -35,3 +35,44 @@ def extrai_naipe(carta):
     
     if "♠" in carta:
         return "♠"
+    
+def extrai_valor(carta):
+        
+    if "A" in carta:
+        return "A"
+    
+    if "2" in carta:
+        return "2"
+    
+    if "3" in carta:
+        return "3"
+    
+    if "4" in carta:
+        return "4"
+
+    if "5" in carta:
+        return "5"
+    
+    if "6" in carta:
+        return "6"
+
+    if "7" in carta:
+        return "7"
+
+    if "8" in carta:
+        return "8"
+
+    if "9" in carta:
+        return "9"
+
+    if "10" in carta:
+        return "10"
+
+    if "J" in carta:
+        return "J"
+
+    if "Q" in carta:
+        return "Q"
+
+    if "K" in carta:
+        return "K"
