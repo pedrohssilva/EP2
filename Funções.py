1261995ad8c5d7fa507caf2647f56c5da101ba43
Pedro Henrique Sousa da Silva <pedrohss1@al.insper.edu.br>
2021-04-27 09:03:12 -0300

    Alterei algumas funções

--- Funções.py
+++ Funções.py
@@ -20,60 +20,19 @@ def cria_baralho():
     return (cartas)
 
 def extrai_naipe(carta):
+    naipes = ["♦", "♥", "♣", "♠"]
+    for naipe in naipes:
+        if naipe in carta:
+            return naipe
 
-    if "♦" in carta:
-        return "♦"
-    
-    if "♥" in carta:
-        return "♥"
-    
-    if "♣" in carta:
-        return "♣"
-    
-    if "♠" in carta:
-        return "♠"
     
 def extrai_valor(carta):
+    
+    valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    for valor in valores:
+        if valor in carta:
+            return valor
         
-    if "A" in carta:
-        return "A"
-    
-    if "2" in carta:
-        return "2"
-    
-    if "3" in carta:
-        return "3"
-    
-    if "4" in carta:
-        return "4"
-
-    if "5" in carta:
-        return "5"
-    
-    if "6" in carta:
-        return "6"
-
-    if "7" in carta:
-        return "7"
-
-    if "8" in carta:
-        return "8"
-
-    if "9" in carta:
-        return "9"
-
-    if "10" in carta:
-        return "10"
-
-    if "J" in carta:
-        return "J"
-
-    if "Q" in carta:
-        return "Q"
-
-    if "K" in carta:
-        return "K"
-    
 def lista_movimentos_possiveis (baralho, indice):
 
     possiveis_movimentos = []
